dcf3aceae9896e9ed3e725071811dc787570470c
radjathaher <33957058+radjathaher@users.noreply.github.com>
2023-10-07 12:56:34 -0400

    Update ApolloProvider.js
    updated vercel

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -9,7 +9,7 @@ import {
 import { setContext } from '@apollo/client/link/context';   
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://merng-server-sigma.vercel.app/'
 });
 
 const authLink = setContext(() => {
